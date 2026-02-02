397a58e76271a708fae10a573f21c71dc2de1b2c
shaketamago <shaketamago.huo7@gmail.com>
2026-02-02 23:00:49 +0900

    課題提出2回目　try～catchを使用ver

--- work/js/app.js
+++ work/js/app.js
@@ -13,12 +13,25 @@ const clearbutton = document.querySelector(".clear");
 
 clearbutton.addEventListener("click",()=>{
   display.value = "";
-})
+  return;
+});
 
 const equalsbutton = document.querySelector(".equals");
 
 equalsbutton.addEventListener("click",()=>{
-  display.value = eval(display.value);
-})
+  if(display.value === ""){
+    return;
+
+  }else if(display.value.includes("**") || display.value.includes("//")){
+    alert("計算が行えません。演算子が連続しています。");
+
+  }else{
+    try{
+      display.value = eval(display.value);
+    }catch(error){
+      alert("計算が行えません。演算子が連続している可能性があります。");
+    }
+  }
+});
 
 
